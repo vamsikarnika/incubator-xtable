91e6caf2fc023ca3470b941c5162d72b7c1a07f1
Jonathan Vexler <=>
2024-02-07 22:24:27 -0500

    disable test

--- core/src/test/java/io/onetable/ITOneTableClient.java
+++ core/src/test/java/io/onetable/ITOneTableClient.java
@@ -58,6 +58,7 @@ import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -460,6 +461,7 @@ public class ITOneTableClient {
                 timestampAndLevelFilter)));
   }
 
+  @Disabled
   @ParameterizedTest
   @MethodSource("provideArgsForPartitionTesting")
   public void testPartitionedData(TableFormatPartitionDataHolder tableFormatPartitionDataHolder) {
